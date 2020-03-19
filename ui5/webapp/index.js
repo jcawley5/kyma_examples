f6cbd32a4635c1b93db18d5ced49e7233f0ff3e5
Jamie Cawley <jamie.cawley@gmail.com>
2020-03-19 18:19:07 -0400

    adding estimator

---
+++ ui5/webapp/index.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/ui/core/mvc/XMLView"], function(XMLView) {
+  "use strict";
+
+  XMLView.create({ viewName: "KymaEstimator.App" }).then(function(oView) {
+    oView.placeAt("content");
+  });
+});
